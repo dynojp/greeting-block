77e45dc699eb41d900d75f13fee49feabb19a8eb
Goto Hayato <gh640@hey.com>
2024-10-17 11:39:08 +0900

    Initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '1d644ede78ec53ccd1f7');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '207507cec3808cd0f061');
